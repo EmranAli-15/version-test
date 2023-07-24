3533880f218df0f990a4e1c27ccc80775c8de6f8
EmranAli-15 <mohammademranali15@gmail.com>
2023-07-25 01:40:40 +0600

    second branch commit

---
+++ src/pages/TwoBranch.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TwoBranch = () => {
+        return (
+                <div>
+                        This is second branch
+                </div>
+        );
+};
+
+export default TwoBranch;
